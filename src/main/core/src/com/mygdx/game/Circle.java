3ed500ccb55278f0d10a143d6d6470f3b2a6bc1b
James Atlas <jatlas@udel.edu>
2014-11-11 11:53:33 -0500

    Added support for Clover and a unit test

---
+++ src/main/core/src/com/mygdx/game/Circle.java
@@ -0,0 +1,30 @@
+package com.mygdx.game;
+
+public class Circle {
+	private double x;
+	private double y;
+	private double radius;
+	public Circle(double x, double y, double radius) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+	}
+	
+	public double getArea() {
+		return this.radius * this.radius * Math.PI;
+	}
+	
+	public void moveTo(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double distanceTo(Circle other) {
+		return Math.sqrt(Math.pow(x - other.x, 2) + Math.pow(y - other.y, 2));
+	}
+	
+	public boolean overlaps(Circle other) {
+		return distanceTo(other) <= (radius + other.radius);
+	}
+}
---
+++ src/main/core/src/com/mygdx/game/test/CircleTest.java
@@ -0,0 +1,26 @@
+package com.mygdx.game.test;
+
+import com.mygdx.game.Circle;
+
+import junit.framework.TestCase;
+
+public class CircleTest extends TestCase {
+	public void test_Circle() {
+		Circle c1 = new Circle(0, 0, 1);
+		Circle c2 = new Circle(1, 1, 1);
+
+		assertEquals(Math.PI, c1.getArea());
+		
+		assertEquals(Math.sqrt(2), c1.distanceTo(c2));
+
+	}
+	public void test_Circle_mutation() {
+		Circle c1 = new Circle(0, 0, 1);
+		
+		Circle c2 = new Circle(1, 1, 1);
+		assertTrue(c1.overlaps(c2));
+		
+		c2.moveTo(5, 5);
+		assertFalse(c1.overlaps(c2));
+	}
+}
